d7e94d9ffac59463704b8fc2b27094bfbdc32255
Sanpi <sanpi@homecomputing.fr>
2023-04-12 12:23:11 +0200

    Adds doc

--- src/lib.rs
+++ src/lib.rs
@@ -13,15 +13,24 @@ pub use serde::*;
 
 pub use errors::{Error, Result};
 
+/**
+ * Loads the *.env* files.
+ */
 pub fn dotenv() {
     dotenvy::dotenv().ok();
 }
 
+/**
+ * Retreives all environment variables as an easy printable form.
+ */
 #[must_use]
 pub fn dump() -> std::collections::HashMap<String, String> {
     std::env::vars().collect()
 }
 
+/**
+ * Likes `try_get` but directly parses the variable value in desired `T` type.
+ */
 pub fn try_parse<T: std::str::FromStr>(key: &str) -> crate::Result<Option<T>>
 where
     T::Err: ToString,
@@ -36,6 +45,9 @@ where
     Ok(Some(value))
 }
 
+/**
+ * Likes `try_parse` but returns a `crate::Error::Missing` if the variable isn’t set.
+ */
 pub fn parse<T: std::str::FromStr>(key: &str) -> crate::Result<T>
 where
     T::Err: ToString,
@@ -43,6 +55,10 @@ where
     crate::try_parse(key)?.ok_or_else(|| crate::Error::Missing(key.to_string()))
 }
 
+/**
+ * Gets the environment variable `key`. This returns a `crate::Error::Unicode` if the variable
+ * value isn’t valid unicode.
+ */
 pub fn try_get(key: &str) -> crate::Result<Option<String>> {
     let Some(value) = std::env::var_os(key) else {
         return Ok(None);
@@ -56,10 +72,16 @@ pub fn try_get(key: &str) -> crate::Result<Option<String>> {
     Ok(Some(value))
 }
 
+/**
+ * Likes `try_get` but returns a `crate::Error::Missing` if the variable isn’t set.
+ */
 pub fn get(key: &str) -> crate::Result<String> {
     crate::try_get(key)?.ok_or_else(|| crate::Error::Missing(key.to_string()))
 }
 
+/**
+ * Sets the environment variable `key` to the `value`.
+ */
 pub fn set<T: ToString>(key: &str, value: T) {
     std::env::set_var(key, value.to_string());
 }
